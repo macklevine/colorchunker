072c1ee1e159fa6834fc3439225e888aae2140ce
macklevine <mackplevine@gmail.com>
2015-09-01 15:26:24 -0700

    refactored to take advantage of get-pixel's ndarray properties.

--- getpixels.js
+++ getpixels.js
@@ -1,86 +1,86 @@
 var getPixels = require('get-pixels');
-var db = require('/db.js');
+var db = require('./db.js');
 
-//NOTES: req.body should have height and width of the image to analyze.
-//for now, we'll use global variables:
+var chunkSize = 1; //ultimately, we want 10.
 
-var height = 5; //250
-var width = 5;  //850
-var chunkSize = 1; //10
-
-//the first argument for getPixels should also be a file path.
+//the first argument for getPixels should also be a file path contained on req.body.path.
 
 var pixelGetter = function(req, res){
+  //eventually, replace path with req.body.path.
   getPixels('fivebyfive.png', function(err, pixels){
     if (err){
       console.log(err);
       return;
     } 
-    chunker(pixels.data, function(_storage){
-      console.log(_storage + "is our storage object");
+    console.log(pixels.shape[0]);
+    console.log(pixels.shape[1]);
+    chunker(pixels, function(_storage){
+      res.send(_storage);
       var Map = new db.Map({
         data: _storage,
-        height: height,
-        width: width
-      })
+        height: pixels.shape[1],
+        width: pixels.shape[0]
+      }).save(function(err, map){
+        res.send(map);
+      });
     });
   });
 };
 
-var chunker = function(data, callback){
+var chunker = function(pixels, callback){
   var _storage = {};
   var index = 0;
-  //image height, image width, and pixel size will be changed according to what we pick for our final spec.
-  for (var i = 0; i < height; i+=chunkSize){ //chunkSize: 2
-    for (var j = 0; j < width; j+=chunkSize){ //chunkSize: 2
-      var chunk = chunkMaker(j, i, data);
+  for (var i = 0; i < pixels.shape[1]; /*height*/ i+=chunkSize){
+    for (var j = 0; j < pixels.shape[0]; /*width*/ j+=chunkSize){
+      var chunk = chunkMaker(j, i, pixels); /*format: x, y, data*/
       _storage[index++] = {
         coords: [j, i], 
-        rgb: returnRGB(chunk),
+        rgb: getAverageColor(chunk),
         original: true
       }
     }
   }
   callback(_storage);
-};
+}
 
-var chunkMaker = function(xCoord, yCoord, data){
+var chunkMaker = function(x, y, pixels){
   var chunk = []; //construct a flat array of pixels scanning left to right, top to bottom.
-  for (var i = yCoord; i < yCoord+chunkSize; i++){
-    for (var j = (xCoord*4); j < ((xCoord*4)+(chunkSize*4)); j++){
-
-      var where = (j + yCoord*width*4);
-      console.log(data[where]);
-      chunk.push(data[where]);
+  //pixels.get(x, y, z)
+  for (var i = y; i < (y + chunkSize); i++){ //outer loop: height.
+    for (var j = x; j < (x + chunkSize); j++){ //inner loop: width:
+      for (var k = 0; k < 4; k++){ //inner-inner loop: depth (rgba values for each pixel)
+        chunk.push(pixels.get(j, i, k));
+      }
     }
   }
   return chunk;
 };
 
-var returnRGB = function(data){
-  return {
-    r: data[0],
-    g: data[1],
-    b: data[2]
-  }
-}
 
-var getAverageColor = function(data) {
+var getAverageColor = function(chunk) {
   var r = 0;
   var g = 0;
   var b = 0;
 
-  for (var i = 0, l = data.length; i < l; i += 4) {
-    r += data[i];
-    g += data[i+1];
-    b += data[i+2];
-  }
+  for (var i = 0, l = chunk.length; i < l; i += 4) {
+    r += chunk[i];
+    g += chunk[i+1];
+    b += chunk[i+2];
+  };
 
-  r = Math.floor(r / (data.length / 4));
-  g = Math.floor(g / (data.length / 4));
-  b = Math.floor(b / (data.length / 4));
+  r = Math.floor(r / (chunk.length / 4));
+  g = Math.floor(g / (chunk.length / 4));
+  b = Math.floor(b / (chunk.length / 4));
 
   return { r: r, g: g, b: b };
 };
+
+// var returnRGB = function(data){
+//   return {
+//     r: data[0],
+//     g: data[1],
+//     b: data[2]
+//   }
+// }
 
 exports.pixelGetter = pixelGetter;
