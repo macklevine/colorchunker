23ac482873ed9e2b81c141cad5dffb7b7db12129
macklevine <mackplevine@gmail.com>
2015-09-01 17:02:01 -0700

    did some more work

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
-var pixelGetter = require('./getpixels.js');
+var db = require('./db.js');
 
 var bodyparser = require('body-parser');
 var path = require('path');
 
 var app = express();
 
+app.use(express.static('./client'));
+app.use(bodyparser.json());
+
 app.listen(3000, function(){
   console.log("listening on port 3000");
 });
 
-app.get('/getpixels', function(req, res){
-  pixelGetter.pixelGetter(req, res);
+app.post('/newevent', function(req, res){
+  console.log(req.data + " is our data");
+  db.saveEvent(req, res);
+});
+
+
+app.get('/', function (req, res) {
+  res.sendfile('./client/index.html');
 });
